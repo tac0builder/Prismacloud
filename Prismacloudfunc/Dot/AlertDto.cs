2a037f218b536211ada4d8b331cb15b27a18f555
Tac0_builder <128030082+tac0builder@users.noreply.github.com>
2024-07-21 11:41:24 +0200

    Add files via upload

---
+++ Prismacloudfunc/Dot/AlertDto.cs
@@ -0,0 +1,16 @@
+﻿namespace PrismaCloudFunc.Dto
+{
+    public class AlertDto
+    {
+        public string Id { get; set; }
+        public string Status { get; set; }
+        public string CloudType { get; set; }
+        public string Severity { get; set; }
+        public string Description { get; set; }
+        public string DescriptionShort { get; set; }
+        public string ResourceType { get; set; }
+        public string ResourceId { get; set; }
+        public string ResourceRegion { get; set; }
+        public string Account { get; set; }
+    }
+}
---
+++ Prismacloudfunc/Dot/TimeDefinition.cs
@@ -0,0 +1,11 @@
+﻿using PrismaCloudFunc.Structures;
+
+namespace PrismaCloudFunc.Dto
+{
+    internal class TimeDefinition
+    {
+        public TimeType TimeType { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public DateTimeOffset EndTime { get; set; }
+    }
+}
